897923b7ccc014cb2cbcef8768e64a35f89793d6
Next-Gen-Coder-2007 <subash59245@gmail.com>
2025-12-31 23:55:39 +0530

    Created the database models for the project

---
+++ backend/src/models/trip.js
@@ -0,0 +1,115 @@
+const mongoose = require("mongoose");
+
+const tripSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  destination: {
+    type: String,
+  },
+  days: {
+    type: Number,
+  },
+  budget: {
+    type: String,
+  },
+  interests: [
+    {
+      type: String,
+    },
+  ],
+  travelStyle: {
+    type: String,
+  },
+  imageURL: {
+    type: String,
+  },
+  aboutTheLocation: [
+    {
+      type: String,
+    },
+  ],
+  stay: [
+    {
+      name: {
+        type: String,
+      },
+      loaction: {
+        type: String,
+      },
+      imageURL: {
+        type: String,
+      },
+      pricePerNight: {
+        type: String,
+      },
+      rating: {
+        type: String,
+      },
+      bookingLink: {
+        type: String,
+      },
+      googleMapLink: {
+        type: String,
+      },
+    },
+  ],
+  plan: [
+    {
+      dayNo: {
+        type: Number,
+      },
+      schedule: [
+        {
+          time: {
+            type: String,
+          },
+          place: {
+            type: String,
+          },
+          activity: {
+            type: String,
+          },
+          imageURL: {
+            type: String,
+          },
+          estimatedCost: {
+            type: String,
+          },
+          googleMapLink: {
+            type: String,
+          },
+        },
+      ],
+    },
+  ],
+  foodRecommendation: [
+    {
+      name: {
+        type: String,
+      },
+      cuisine: {
+        type: String,
+      },
+      rating: {
+        type: String,
+      },
+      imageURL: {
+        type: String,
+      },
+      location: {
+        type: String,
+      },
+      googleMapLink: {
+        type: String,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Trip', tripSchema)
---
+++ backend/src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  image: {
+    type: Text,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
