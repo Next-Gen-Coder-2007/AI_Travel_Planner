8929195e70c9606b484b535c6df8043f8e43e611
Next-Gen-Coder-2007 <subash59245@gmail.com>
2026-01-08 15:20:44 +0530

    Some Small Changes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -36,7 +36,9 @@ const loginUser = async (req, res) => {
     const token = generateToken(user._id)
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: true,  
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
       message: "Login successful",
